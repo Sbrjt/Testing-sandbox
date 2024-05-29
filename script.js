52d072a1a3f635ace2dffb7a161a1f9f06d953df
Sbrjt <champa.281982@gmail.com>
2024-05-29 21:04:53 +0530

    ...

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-;async () => {
+;(async () => {
 	const response = await fetch('data.db')
 	const buffer = await response.arrayBuffer()
 	const db = new SQL.Database(new Uint8Array(buffer))
 	db.prepare('select * from tb')
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-	const btn = document.getElementById('btn')
-	const box = document.getElementById('box')
-	const msg = document.getElementById('msg')
 
 	btn.addEventListener('click', () => {
-		localStorage.setItem('txt', box.value)
-	})
+		t = Array.from(document.getElementsByName('type'))
+			.filter((i) => i.checked)
+			.map((i) => `'${i.value}'`)
+			.join(', ')
 
-	if (localStorage.getItem('txt') !== null) {
-		const x = localStorage.getItem('txt')
-		msg.innerHTML = x
-		box.value = x
-	}
+		localStorage.setItem('rank', rank.value)
+		localStorage.setItem('category', category.value)
+		localStorage.setItem('state', state.value)
+		localStorage.setItem('type', t)
+	})
+})()
+
+document.addEventListener('DOMContentLoaded', () => {
+	// load form inputs from local storage
+	;(function load_local() {
+		if (localStorage.getItem('rank') !== null) {
+			document.getElementById('rank').value = localStorage.getItem('rank')
+			document.getElementById('category').value = localStorage.getItem('category')
+			document.getElementById('state').value = localStorage.getItem('state')
+			const type = localStorage.getItem('type').slice(1, -1).split("', '")
+			for (let i of Array.from(document.getElementsByName('type'))) {
+				if (!type.includes(i.id)) {
+					i.checked = false
+				}
+			}
+		}
+	})()
 })
