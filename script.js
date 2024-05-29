3e30823633d7bd46cef73ad55f818e083c6cd362
Sbrjt <champa.281982@gmail.com>
2024-05-29 23:23:57 +0530

    ...

--- script.js
+++ script.js
@@ -1,76 +1,21 @@
 ;(async () => {
 	const btn1 = document.getElementById('btn-1')
 
-	let rank = 0,
-		category,
-		branch,
-		state,
-		gender,
-		type,
-		result,
-		rowCount,
-		row
-
-	// fetch data and insert the first 10 records into table on clicking btn
 	btn1.addEventListener('click', () => {
-		rank = document.getElementById('rank').value
-		// validating rank input with regex (only digits allowed)
-		if (!/^\d+$/.test(rank)) {
-			result = []
-			return
-		}
+		const txt = document.getElementById('txt').value
+		const type = document.getElementById('type').value
 
-		category = document.getElementById('category').value
-
-		// // Getting selected values from 'branch' and join them (with commas and quotes) to prepare SQL query
-		// branch = Array.from(document.getElementById('branch').selectedOptions)
-		// 	.map((i) => `'${i.value}'`)
-		// 	.join(', ')
-
-		// state = document.getElementById('state').value
-
-		// gender = Array.from(document.getElementsByName('gender'))
-		// 	.filter((i) => i.checked)
-		// 	.map((i) => `'${i.value}'`)
-		// 	.join(', ')
-
-		// // get elements with name 'type', convert to array, filter to get only the checked ones,
-		// // map them to an array of their values wrapped in single quotes,
-		// // join the array into a comma-separated string to prepare SQL query
-		// type = Array.from(document.getElementsByName('type'))
-		// 	.filter((i) => i.checked)
-		// 	.map((i) => `'${i.value}'`)
-		// 	.join(', ')
-	})
-
-	// save values to local storage
-	window.addEventListener('beforeunload', () => {
-		if (rank) {
-			localStorage.setItem('rank', rank)
-			localStorage.setItem('category', category)
-			// localStorage.setItem('state', state)
-			// localStorage.setItem('gender', gender)
-			// localStorage.setItem('type', type)
-		}
+		localStorage.setItem('txt', txt)
+		localStorage.setItem('type', type)
 	})
 })()
 
 document.addEventListener('DOMContentLoaded', () => {
 	// load form inputs from local storage
 	;(function load_local() {
-		if (localStorage.getItem('rank') !== null) {
-			document.getElementById('rank').value = localStorage.getItem('rank')
-			document.getElementById('category').value = localStorage.getItem('category')
-			// document.getElementById('state').value = localStorage.getItem('state')
-
-			// const g = localStorage.getItem('gender').slice(1, -1).split("', '")
-
-			// for (let i of Array.from(document.getElementsByName('gender'))) {
-			// 	i.checked = g.includes(i.id)
-			// }
-
-			// document.getElementById('neu-radio').checked = document.getElementById('Neutral').checked
-			// document.getElementById('fem-radio').checked = document.getElementById('Female').checked
+		if (localStorage.getItem('txt') !== null) {
+			document.getElementById('txt').value = localStorage.getItem('txt')
+			document.getElementById('type').value = localStorage.getItem('type')
 		}
 	})()
 })
